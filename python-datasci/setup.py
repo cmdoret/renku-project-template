56bbec4569fa66c539d74debe767e513741ff1d6
cmdoret <cyril.mattheydoret@gmail.com>
2022-06-20 15:46:25 +0200

    add templated pkg dir in python-datasci

--- python-datasci/setup.py
+++ python-datasci/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="src",
+    name='{{ __sanitized_project_name__ | replace("-", "_")}}',
     version="0.1.0",
     packages=find_packages(),
 )
